4471dc60ea1578ebe962490d6abf8a94f20f59ed
Imtiaz Mahmud Sobuj <sobujedu@gmail.com>
2026-03-19 17:06:09 +0600

    Add files via upload

---
+++ GRAPH_ALGORITHMS/dfs_on_2d_grid.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+char grid[105][105];
+bool vis[105][105];
+int n,m;
+vector<pair<int,int>>mov = {{-1,0},{1,0},{0,-1},{0,1}};
+bool valid(int i, int j)
+{
+    if(i<0 || i >=n || j<0 || j>=m)
+      return false;
+    return true;
+   
+}
+void dfs(int si,int sj)
+{
+   
+    cout << si << " " << sj << endl;
+    vis[si][sj] = true;
+    for(int i=0;i<4;i++)
+    {
+        int ci,cj;
+        ci = si + mov[i].first;
+        cj = sj + mov[i].second;
+        if(valid(ci,cj)==true && !vis[ci][cj])
+           dfs(ci,cj);
+    }
+}
+int main()
+{
+    cin >> n >> m;
+    for(int i=0;i<n;i++)
+    {
+       for(int j=0;j<m;j++)
+       {
+           cin >> grid[i][j];
+       }
+    }
+    int si,sj;
+    cin >> si >> sj;
+    memset(vis,false,sizeof(vis));
+    dfs(si,sj);
+    return 0;
+}
